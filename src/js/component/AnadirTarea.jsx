c1f509a5ff1d143ea1d49352293f91a67acf155b
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-06-17 18:28:19 +0000

    modificacion añadir tarea, mostrar tarea y eliminar tarea con API

--- src/js/component/AnadirTarea.jsx
+++ src/js/component/AnadirTarea.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import '../../styles/anadirTarea.css';
+import { anadirTareaService } from "../services/services";
+import { obtenerTareasServices } from "../services/services";
+import { eliminarTareaService } from "../services/services";
 
 const AnadirTarea = () => {
 
@@ -7,25 +10,61 @@ const AnadirTarea = () => {
     const [listaTareas, setListaTareas] = useState([]); //creamos la variable que almanecena el listado de tareas en un array vacio
     const [sinTareas, setSinTareas] = useState(false);
     const [totalTareas, setTotalTareas] = useState(0);
-
+    const usuario_servicio = 'tom';
     const recogerTarea = (evento) => {
         setAnadirTarea(evento.target.value);
     }
 
 
     const pulsarIntro = (evento) => { //evento: usado  para cuando ocurre un evento en el navegador, en este caso referido a una tecla.
-        setAnadirTarea(evento.target.value) //actualiza el estado anadirTarea cada vez que se ejecute una tecla (e.t ->input donde esta escribiendo / e.t.v --lo que contiene el input)
-        if (evento.key === 'Enter' && evento.target.value !== '') { //si la tecla es Enter y el input no está vacío...
-            setListaTareas([...listaTareas, evento.target.value]) //...añade la tarea al array listaTareas
-            //crea copia array , añade el valor del input        
-            setAnadirTarea("") //deja el input vacío
+         //actualiza el estado anadirTarea cada vez que se ejecute una tecla (e.t ->input donde esta escribiendo / e.t.v --lo que contiene el input)
+        if (evento.key === 'Enter' && anadirTarea.trim() !== '') { //si la tecla es Enter y el input no está vacío...
+            anadirTareaService(usuario_servicio, anadirTarea)
+            .then (nuevaTarea => {
+                setListaTareas([...listaTareas, nuevaTarea]);
+                setAnadirTarea("");
+            })
+            .catch(error => {
+                console.log("Error al añadir tarares: "+error);
+            });
+        
+
+            // //-----------------------------------------------------------------------------------------------//
+            // //¡¡API!! --> se añade el primer servicio que es añadirTarea. Se importa arriba. 
+            // //recibe mi nombre de usuario (tom) y la tarea que recibe en la linea 19.
+            // anadirTareaService(usuario_servicio, anadirTarea)
+            // //-----------------------------------------------------------------------------------------------//
+            // setAnadirTarea("") //deja el input vacío
         }
     };
+    ////////////////////////////////////// API  MOSTRAR TAREAS ///////////////////////////////////////////////////
+    useEffect(() => {
+        // Obtiene las tareas al montar el componente
+        obtenerTareasServices(usuario_servicio)
+            .then(data => {
+                if (Array.isArray(data.todos)) {
+                    setListaTareas(data.todos);
+                } else {
+                    console.error("La respuesta no es un array de tareas:", data);
+                }
 
-    const eliminarTarea = (tarea) => {
-        setListaTareas(listaTareas.filter((_, i) => i !== tarea));
+            })
+            .catch(error => {
+                console.error("Error al obtener tareas:", error);
+            });
+    }, [usuario_servicio]);
+
+
+    ////////////////////////////////////// API  ELIMINAR UNA TAREA ///////////////////////////////////////////////////
+    const eliminarTarea = (todo_id) => {
+        eliminarTareaService(todo_id)
+            .then(() => {
+                setListaTareas(listaTareas.filter(tarea => tarea.id !== todo_id));
+            })
+            .catch(error => {
+                console.error("Error al eliminar tarea:", error);
+            });
     }
-
 
 
     //Contador de tareas
@@ -49,25 +88,25 @@ const AnadirTarea = () => {
                 </div>
                 {listaTareas.length === 0 ? (<p className="mt-2 ms-2">No hay tareas, agrega una</p>) : (
                     <ul>
-                        {listaTareas.map((tareaNueva, index) => (
-                            <li className="enTarea" key={index}>{tareaNueva}
+                        {listaTareas.map((tareaNueva) => (
+                            <li className="enTarea" key={tareaNueva.id}>{tareaNueva.label}
                                 <span
                                     className="close-icon"
-                                    onClick={() => eliminarTarea(index)}
+                                    onClick={() => eliminarTarea(tareaNueva.id)}
                                 >
                                     X
                                 </span>
                                 <hr />
                             </li>
-                            
+
                         ))}
                     </ul>
                 )}
                 <p className="contador">Total tareas: {totalTareas}</p>
             </div>
-            
+
         </div>
     );
-}    
+}
 
 export default AnadirTarea;
---
+++ src/js/services/services.js
@@ -0,0 +1,98 @@
+// services/services.js
+const ApiUrl = 'https://playground.4geeks.com/todo';
+const postToDo = '/todos/';
+const obtener = '/users/'
+
+/******************************
+ * 
+ *      CONEXIÓN API 
+ * 
+ *****************************/
+
+
+/**
+ * Función API para añadir una tarea. 
+ * @param {user_name } nombre de usuario que permite la conexion a la api
+ * @param {label} es la tarea que se recibe cuando el usuario agrega una tarea. 
+ * @returns devuelve: id, label, is_done
+ */
+const anadirTareaService = (user_name, label) => {
+    return fetch(`${ApiUrl}${postToDo}${user_name}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user_name, label }), //stringify - convierte los string a formato json // En la api aparece como requerido
+    })
+        .then(respuesta => {
+            console.log(respuesta);
+            if (!respuesta.ok) {
+                throw new Error('No fue ok ' + respuesta.statusText)
+            } else {
+                return respuesta.json()
+            }
+        })
+        .then(datosRespuesta => {
+            console.log(datosRespuesta);
+        })
+        .catch(esError => {  //catch captura el error del if si fueserroneo, no es obligatorio pero si buena práctica
+            console.log('Error: ' + esError);
+        })
+};
+
+export { anadirTareaService };
+
+
+const obtenerTareasServices = (user_name) => {
+    return fetch(`${ApiUrl}${obtener}${user_name}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(respuesta => {
+            console.log(respuesta);
+            if (!respuesta.ok) {
+                throw new Error('No fue ok ' + respuesta.statusText)
+            }
+            return respuesta.json()
+
+        })
+        .then(datosRespuesta => {
+            console.log(datosRespuesta);
+            return datosRespuesta;
+        })
+        .catch(esError => {  //catch captura el error del if si fueserroneo, no es obligatorio pero si buena práctica
+            console.log('Error: ' + esError);
+        })
+};
+
+export { obtenerTareasServices };
+
+
+const eliminarTareaService = (todo_id) => {
+    return fetch(`${ApiUrl}${postToDo}${todo_id}`, {
+        method: 'DELETE',
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // }
+    })
+    .then(respuesta => {
+        if (!respuesta.ok) {
+            throw new Error('No fue ok ' + respuesta.statusText)
+        }
+        return respuesta.json()
+
+    })
+    .then(datosRespuesta => {
+        console.log(datosRespuesta);
+        return datosRespuesta;
+    })
+    .catch(esError => {  
+        console.log('Error: ' + esError);
+    })
+}
+export  {eliminarTareaService}
+
+
+
